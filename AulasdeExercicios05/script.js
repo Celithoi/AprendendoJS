56a99aaec2a36a3ef93b7fddfe692aadaae45522
Marcelo Henrique Rocha Cardoso <mhrcardoso@gmail.com>
2022-05-30 22:49:40 -0300

    finalizado o curso em video no youtube
    finalmente terminando o curso de java script do curso em video no youtube

--- AulasdeExercicios05/script.js
+++ AulasdeExercicios05/script.js
@@ -3,6 +3,17 @@ let lista = document.querySelector('select#resultados')
 let res = document.querySelector('div#res')
 let valores = []
 
+ //testando pegar o Enter como evento
+var c = document.getElementById('entrada')
+
+c.addEventListener("keypress",(event)=>{
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById('entrar').click();
+        
+    }
+});
+
 function isNumber(n){
     if(Number(n) >= 1 && Number(n)<= 100){
         return true
@@ -13,7 +24,7 @@ function inlista(n,l){
     if (l.indexOf(Number(n)) != -1){
         return true
     }else{return false}
-}
+}   
 
 
 function entradaDeDados(){
@@ -23,6 +34,7 @@ function entradaDeDados(){
         let item = document.createElement('option')
         item.text = `Valor ${entrada.value} adicionado` 
         lista.appendChild(item)
+        res.innerHTML = ``
     }else{
         window.alert('valor não existe ou esta na lista') 
 
@@ -35,5 +47,29 @@ function entradaDeDados(){
 
 function validarDados(){
 
+    if (valores.length == 0){
+        window.alert('Adicione valores antes de Finalizar')
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+        for (let pos in valores){
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / total
+        res.innerHTML = ``
+        res.innerHTML += `<p> Ao todo, temos ${total} numeros cadastrados.<p>`
+        res.innerHTML += `<p> O maior valor informado foi ${maior}. </p>`
+        res.innerHTML += `<p> O menor valor informado foi ${menor}. </p>`
+        res.innerHTML += `<p> A soma dos valores informados foi ${soma}.`
+        res.innerHTML += `<p> A media dos valores informados e igual a ${media}. </p>`
+    }
 
 }
