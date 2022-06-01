e3d400cd3f869a9dc3b791d4cc22abaaebd8e49a
Marcelo Henrique Rocha Cardoso <100224714+Celithoi@users.noreply.github.com>
2022-06-01 00:03:10 -0300

    começando o js
    começando o java Script

---
+++ Calculadora/script.js
@@ -0,0 +1,5 @@
+const previousOperationText = document.querySelector("#operacaoAnterior")
+const currentOperationText = document.querySelector("#operacaoAtual")
+const buttons = document.querySelectorAll("#container button")
+
+console.log("buttons")
